1fad21522acf980cc8065f5a601a88f89ae3c1b6
Gabe Masiulis <gabe@codeofthenorth.com>
2018-10-26 14:26:54 -0500

    adding untested updateData function

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ def receive_post():
     if request.json is None:
         return "Please Submit JSON", 400
     postJson = request.json
-    saveData(postJson)
+#     saveData(postJson)
+    updateData(postJson, openData())
     for element in postJson['hello']:
         print(element)
     print(round_time_object(datetime.now()))
@@ -31,6 +32,25 @@ def saveData(obj):
 def openData():
     with open('./data/temp.pkl', 'rb') as f:
         return pickle.load(f)
+
+def updateData(newObj, data):
+    print('updating data. Before:') 
+    print(data)   
+    timestampNow = round_time_object(datetime.now())
+    '''
+    Remove data entries that are greater than 24 hours
+    '''
+    newData = list(filter(lambda entry: (timestampNow - timedelta(days=1) > entry['timestamp']), data['data']))
+    data['data'] = newData
+    newObj['timestamp'] = timestampNow
+    data['data'].append(newObj)
+    print('after:')
+    print(data)
+    with open('./data/temp.pkl', 'wb') as f:
+        pickle.dump(newObj, f, pickle.HIGHEST_PROTOCOL)
+# def entryComparison(entry):
+#     timestampNow = round_time_object(datetime.now())
+    
 
 def round_time_object(timeObject):
     newTimeObject = datetime(timeObject.year, timeObject.month, timeObject.hour, round(60 / int(timeObject.minute)))
